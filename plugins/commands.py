b2e9d0dde1bc508f24b2d89210b2b151c56a2efe
boszsz <110412923+boszsz@users.noreply.github.com>
2022-08-21 09:49:53 -0700

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -237,7 +237,7 @@ async def start(client, message):
             return
         except:
             pass
-        return await message.reply('No such file exist.')
+        return await message.reply('No such file exist.ഇ ഫയൽ ഇപ്പോൾ ലഭ്യമല്ല 🌝')
     files = files_[0]
     title = files.file_name
     size=get_size(files.file_size)
